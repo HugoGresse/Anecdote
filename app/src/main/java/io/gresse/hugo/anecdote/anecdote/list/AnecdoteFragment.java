b7ca931dadc3a5e2a5d6a921d3fd6884371db186
Hugo Gresse <hugo.gresse@gmail.com>
2016-11-22 13:23:51 +0100

    Remove DiffUtil unused import

--- app/src/main/java/io/gresse/hugo/anecdote/anecdote/list/AnecdoteFragment.java
+++ app/src/main/java/io/gresse/hugo/anecdote/anecdote/list/AnecdoteFragment.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.preference.PreferenceManager;
-import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
