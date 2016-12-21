5227d236e76cbbe9363c02f477162b2c09b56531
Hugo Gresse <hugo.gresse@gmail.com>
2016-12-21 13:55:41 +0100

    Move restore websites to advanced settings

--- app/src/main/java/io/gresse/hugo/anecdote/setting/SettingsFragment.java
+++ app/src/main/java/io/gresse/hugo/anecdote/setting/SettingsFragment.java
@@ -2,13 +2,17 @@ package io.gresse.hugo.anecdote.setting;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.View;
 
 import org.greenrobot.eventbus.EventBus;
 
 import io.gresse.hugo.anecdote.R;
 import io.gresse.hugo.anecdote.anecdote.UpdateAnecdoteFragmentEvent;
 import io.gresse.hugo.anecdote.event.ChangeTitleEvent;
+import io.gresse.hugo.anecdote.event.ResetAppEvent;
 import io.gresse.hugo.anecdote.tracking.EventTracker;
 
 /**
@@ -21,6 +25,20 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.pref_general);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Preference myPref = findPreference(getString(R.string.pref_reset_key));
+        myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                EventBus.getDefault().post(new ResetAppEvent());
+                return true;
+            }
+        });
+
     }
 
     @Override
