4f85719b3bf4acc91b1fc494b985394e218f09f7
Hugo Gresse <hugo.gresse@gmail.com>
2017-06-25 19:49:34 +0200

    Add ObjectBox Toothpick module in its own class

--- app/src/main/java/io/gresse/hugo/anecdote/AnecdoteApplication.java
+++ app/src/main/java/io/gresse/hugo/anecdote/AnecdoteApplication.java
@@ -3,10 +3,8 @@ package io.gresse.hugo.anecdote;
 import android.app.Application;
 
 import io.gresse.hugo.anecdote.anecdote.model.MyObjectBox;
-import io.objectbox.BoxStore;
 import toothpick.Scope;
 import toothpick.Toothpick;
-import toothpick.config.Module;
 import toothpick.registries.FactoryRegistryLocator;
 import toothpick.registries.MemberInjectorRegistryLocator;
 import toothpick.smoothie.module.SmoothieApplicationModule;
@@ -28,16 +26,12 @@ public class AnecdoteApplication extends Application {
         MemberInjectorRegistryLocator.setRootRegistry(new io.gresse.hugo.anecdote.MemberInjectorRegistry());
         FactoryRegistryLocator.setRootRegistry(new io.gresse.hugo.anecdote.FactoryRegistry());
 
-        // Create BoxStore and supply to to Toothpick to DI
-        final BoxStore boxStore = MyObjectBox.builder().androidContext(this).build();
-
         // Open and attach smoothie module on Toothpick DI library
         Scope appScope = Toothpick.openScope(this);
         appScope.installModules(new SmoothieApplicationModule(this));
-        appScope.installModules();
-
-        appScope.installModules(new Module() {{
-            bind(BoxStore.class).toInstance(boxStore);
-        }});
+        // Create BoxStore and supply to to Toothpick to DI
+        appScope.installModules(new ObectBoxModule(
+                MyObjectBox.builder().androidContext(this).build()
+        ));
     }
 }
